1595b5e045bfabbffbf0e47f5a2a2c5f8a669a68
DinizEduardo <eduardo.diniz987@gmail.com>
2021-02-11 09:27:12 -0300

    Testando o controller de Autenticação

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -19,7 +19,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
-@Profile("prod")
+@Profile(value = {"prod", "teste"})
 public class AutenticacaoController {
 
     @Autowired
